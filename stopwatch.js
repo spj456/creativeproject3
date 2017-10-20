8081054635358ffaba022b50bd53ea483dad6175
spj456 <spj432@gmail.com>
2017-10-19 20:15:41 -0600

    initial commit

--- stopwatch.js
+++ stopwatch.js
@@ -1,45 +1,37 @@
-angular.module('app', [])
-  .controller('mainCtrl', mainCtrl)
-  .directive('avatar', avatarDirective);
+var app = angular.module("myApp", []);
+app.controller("myController", function ($scope, $timeout, $interval) {
 
-function mainCtrl ($scope) {
+    $scope.timerWithTimeout = 0;
+    $scope.startTimerWithTimeout = function () {
+        $scope.timerWithTimeout = 0;
+        if ($scope.myTimeout) {
+            $timeout.cancel($scope.myTimeout);
+        }
+        $scope.onTimeout = function () {
+            $scope.timerWithTimeout++;
+            $scope.myTimeout = $timeout($scope.onTimeout, 1000);
+        }
+        $scope.myTimeout = $timeout($scope.onTimeout, 1000);
+    };
 
-  $scope.users = [];
-
-  $scope.addNew = function (user) {
-    $scope.users.push({ 
-      name: user.name,
-      email: user.email,
-      avatarUrl: user.url
-    });
-    
-    user.name = '';
-    user.email = '';
-    user.url = '';
-  };
-}
-
-function avatarDirective () {
-  return {
-    scope: {
-      user: '='
-    },
-    restrict: 'E',
-    replace: 'true',
-    template: (
-      '<div class="Avatar">' +
-        '<img ng-src="{{user.avatarUrl}}" />' +
-        '<h4>{{user.name}}</h4>' +
-        '<h2>{{user.email}}</h2>' +
-      '</div>'
-    ),
-    link: link
-  };
-  
-  function link (scope) {
-    if (!scope.user.avatarUrl) {
-      scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
+    $scope.resetTimerWithTimeout = function () {
+        $scope.timerWithTimeout = 0;
+        $timeout.cancel($scope.myTimeout);
     }
-  }
+})
+app.filter('hhmmssmm', function () {
+    return function (time) {
+        var sec_num = parseInt(time, 10);
+        var hours = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+        var milliseconds = sec_num - (hours * 3600) - (minutes * 60) - (seconds * 60);
 
-}
+        if (hours < 10) { hours = "0" + hours; }
+        if (minutes < 10) { minutes = "0" + minutes; }
+        if (seconds < 10) { seconds = "0" + seconds; }
+        if (milliseconds < 10) { milliseconds = "0" + milliseconds; }
+        var time = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+        return time;
+    }
+});
