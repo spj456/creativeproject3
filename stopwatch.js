9419eae11e34083d8cc844804c2297e18d695740
Mike Crowther <macrowther7+git@gmail.com>
2017-10-19 21:44:00 -0600

    milliseconds attempt

--- stopwatch.js
+++ stopwatch.js
@@ -20,12 +20,17 @@ app.controller("myController", function ($scope, $timeout, $interval) {
     }
 })
 app.filter('hhmmssmm', function () {
-    return function (time) {
-        var sec_num = parseInt(time, 10);
-        var hours = Math.floor(sec_num / 3600);
-        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = sec_num - (hours * 3600) - (minutes * 60);
-        var milliseconds = sec_num;
+    return function (duration) {
+        // var sec_num = parseInt(time, 10);
+        // var hours = Math.floor(sec_num / 3600);
+        // var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        // var seconds = sec_num - (hours * 3600) - (minutes * 60);
+        // var milliseconds = sec_num;
+
+        var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
 
         if (hours < 10) {
             hours = "0" + hours;
@@ -36,10 +41,10 @@ app.filter('hhmmssmm', function () {
         if (seconds < 10) {
             seconds = "0" + seconds;
         }
-        if (milliseconds < 10) {
-            milliseconds = "0" + milliseconds;
-        }
-        var time = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+        // if (milliseconds < 10) {
+        //     milliseconds = "0" + milliseconds;
+        // }
+        var time = hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
         return time;
     }
 });
