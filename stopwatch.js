f6d72001eb5cf7be5df2ddacf93a0cc998dc96f6
Mike Crowther <macrowther7+git@gmail.com>
2017-10-19 21:01:10 -0600

    more styles

--- stopwatch.js
+++ stopwatch.js
@@ -27,10 +27,18 @@ app.filter('hhmmssmm', function () {
         var seconds = sec_num - (hours * 3600) - (minutes * 60);
         var milliseconds = sec_num;
 
-        if (hours < 10) { hours = "0" + hours; }
-        if (minutes < 10) { minutes = "0" + minutes; }
-        if (seconds < 10) { seconds = "0" + seconds; }
-        if (milliseconds < 10) { milliseconds = "0" + milliseconds; }
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (milliseconds < 10) {
+            milliseconds = "0" + milliseconds;
+        }
         var time = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
         return time;
     }
